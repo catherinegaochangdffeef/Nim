36fa111214d74dc6895a026275ffa46c4a3f157a
catherinegaochangdffeef <56112613+catherinegaochangdffeef@users.noreply.github.com>
2020-01-19 13:51:18 +0100

    Update C

--- fonctions.c
+++ fonctions.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-#include "fonctions.h"
-
-
+#include "entetes.h"
+#define VMIN 5
+#define VMAX 30
 
 int Lire_Entier(int bi, int bs)
 {
@@ -12,39 +12,26 @@ int Lire_Entier(int bi, int bs)
     {
         scanf("%d", &n);
     }
-    while ((n<bi)||(n>bs));
+    while (( n < bi) || (n > bs));
     return n;
 }
 
-int caseBannie(struct T_Case c, T_Tab_Case tab)
-{
-    int i;
-    for(i=0; i<tab.taille; i++)
-    {
-        if(tab.tab[i].x==c.x && tab.tab[i].y==c.y)
-            return 1;
-    }
-    return 0;
-
-}
-
-void Parametres(int *pointeurnlig, int *pointeurncol, int *pointeurnban, int *pointeurniveau, int *pointeurnext)
+void Parametres(int *nlig, int *ncol, int *nban, int *niveau, int *next)
 {
     printf("Entrez un nombre de lignes compris entre 5 et 30: " );
-    *pointeurnlig=Lire_Entier(VMIN,VMAX);
+    *nlig=Lire_Entier(VMIN,VMAX);
     printf("Entrez un nombre de colonnes compris entre 5 et 30: ");
-    *pointeurncol=Lire_Entier(VMIN,VMAX);
+    *ncol=Lire_Entier(VMIN,VMAX);
     printf("Nombre de cases bannies generees: ");
-    *pointeurnban=Hasard(max(*pointeurnlig, *pointeurncol));
+    *nban=Hasard(max(*nlig, *ncol));
     printf("Niveau de difficulte de 1 a 4: ");
-    *pointeurniveau=Lire_Entier(1,4);
+    *niveau=Lire_Entier(1,4);
     printf("Qui commence?\nL'ordinateur (1) ou le joueur (2): ");
-    *pointeurnext=Lire_Entier(1,2);
+    *next=Lire_Entier(1,2);
 }
 
 int Hasard(int val)
 {
-    srand(time(NULL));
     int nb=rand()%val;
     printf("%d\n",nb);
     return nb;
@@ -56,55 +43,55 @@ int max(int a, int b)
     else return b;
 }
 
-void Hasard_Ban(int nlig, int ncol, T_Tab_Case T, int aBannir)
+void Hasard_Ban(int nlig, int ncol, int nban, T_Tab_Case *T)
 {
     struct T_Case _case;
+    _case.x=ncol;
+    _case.y=nlig;
     int k, test;
-    T.taille=0;
-    for (k = 0; k < aBannir; k++)
+    for (k = 0; k < nban; k++)
     {
         do
         {
-            _case.x=rand()%nlig;
-            _case.y=rand()%ncol;
-            test=Bannissable(nlig, ncol, T, _case);
+            _case.x=rand()%nlig+1;
+            _case.y=rand()%ncol+1;
+            test=Bannissable(k, nlig, ncol, T, _case);
         }
         while(test==0);
-        T.tab[k]=_case;
-        T.taille++;
+        (*T).tab[k].x=_case.x;
+        (*T).tab[k].y=_case.y;
     }
 
 }
-//Fonction qui d�finit les contraintes des cases bannies
-int Bannissable(int nlig, int ncol, T_Tab_Case T, struct T_Case _case) 
+
+int Bannissable(int k, int nlig, int ncol, T_Tab_Case *T, struct T_Case _case)
 {
-    int bannissable;
-    //si banissable=0, pas bannissable
-    bannissable=0;
-    if(_case.y == 0 && _case.x == ncol-1)
+    int m, bannissable, diagonal;
+    bannissable=0; //Si banissable=0, pas bannissable
+    diagonal=0; //Si diagonal=1, bannissable
+    if(_case.y == 1 && _case.x == ncol) //coin en bas � gauche
         bannissable=1;
-    else if(_case.y == nlig-1 && _case.x == 0)
+    else if(_case.y == nlig && _case.x == 1) //coin en haut � droite
         bannissable=1;
-    else if(_case.x > 0 &&_case.x < ncol-1 && _case.y < nlig-1 && _case.y > 0 )
+    else if(_case.x > 1 && _case.y < nlig && _case.y > 1 && _case.x < ncol) //hors bordures
         bannissable=1;
-    if (caseBannie(_case, T))
-        bannissable = 0;
-    struct T_Case caseTmp = _case;
-    caseTmp.x++;
-    caseTmp.y--;
-    if (caseBannie(caseTmp, T))
-        bannissable = 0;
-    caseTmp = _case;
-    caseTmp.x--;
-    caseTmp.y++;
-    if (caseBannie(caseTmp, T))
-        bannissable = 0;
+    //else bannissable=0;
+    for(m = 0; m < k; m++)
+    {
+        if (_case.y==(*T).tab[m].y+1 && _case.x==(*T).tab[m].x-1) //diagonale haut droite
+            diagonal=1;
+        else if (_case.y==(*T).tab[m].y-1 && _case.x==(*T).tab[m].x+1) //diagonale bas gauche
+            diagonal=1;
+        else if (_case.y==(*T).tab[m].y && _case.x==(*T).tab[m].x) //case bannie sur elle-m�me
+            diagonal=1;
+    }
+    if(diagonal==1) bannissable=0;
     return bannissable;
 }
 
-void AfficheGrille(int nlig,int ncol, T_Tab_Case T, struct T_Case pion)
+void AfficheGrille(int nlig, int ncol, int nban, T_Tab_Case *T, struct T_Case pion)
 {
-    int i, j;
+    int i, j, k, case_deja_faite;
     printf ("   ");
     for(i=1;i<= ncol;i++)
     {
@@ -112,78 +99,51 @@ void AfficheGrille(int nlig,int ncol, T_Tab_Case T, struct T_Case pion)
 
     }
     printf("\n");
-    for(i=0;i< nlig;i++)
+    for(j=1;j<= nlig;j++)
     {
-        if(i<10)
-            printf("%d |",i+1);
-        else
-            printf("%d|", i+1);
-        for(j=0;j< ncol;j++)
+        if(j < 10) printf("%d |",j);
+        else printf("%d|", j);
+        for(i=1;i<= ncol;i++)
         {
-            struct T_Case curr;
-            curr.x = j;
-            curr.y = i;
+            case_deja_faite=0;
             if (j==pion.x && i==pion.y)
-                printf(" O |");            
-            else if (caseBannie(curr, T))
-                printf(" X |");            
+                {
+                    printf(" O |");
+                    case_deja_faite=1;
+                }
+            for(k=0;k<=nban;k++)
+            {
 
-            else
-                printf(" - |");
+                if ((j==(*T).tab[k].x) && (i==(*T).tab[k].y))
+                {
+                    printf (" X |");
+                    case_deja_faite=1;
+                }
+
+
+
+            }
+            if (case_deja_faite==0) printf(" - |");
 
         }
         printf("\n");
     }
 }
 
-void Voisines(int nlig, int ncol, T_Tab_Case *t, T_Tab_Case ban, struct T_Case _case)
-{
-    struct T_Case caseDroite, caseDroiteDroite, caseBas, caseBasBas;
-    t->taille=0;
-    caseDroite = caseDroiteDroite = caseBas = caseBasBas = _case;
-    caseDroite.x=_case.x+1;
-    caseBas.y=_case.y+1;
-    caseBasBas.y=_case.y+2;
-    caseDroiteDroite.x=_case.x+2;
-    if(!caseBannie(caseDroite, ban) && caseDroite.x < ncol)
-    {
-        t->tab[t->taille] = caseDroite;
-        t->taille++;
 
-        if(!caseBannie(caseDroiteDroite, ban) && caseDroiteDroite.x < ncol)
-        {
-            t->tab[t->taille] = caseDroiteDroite;
-            t->taille++;
-            
-        }
-
-    }
-    if(!caseBannie(caseBas, ban) && caseBas.y < nlig)
-    {
-        t->tab[t->taille] = caseBas;
-        t->taille++;
-        if(!caseBannie(caseBasBas, ban) && caseBasBas.y < nlig)
-        {
-            t->tab[t->taille] = caseBasBas;
-            t->taille++;
-            
-        }
-    }
-}
-
-
-void Calcul_Nimber(int nim[][VMAX],int nlig,int ncol)
+//remplacer la table par 0,1,2,3
+void Calcul_Nimbers(int nim[][VMAX], int nlig, int ncol)
 {
     int x,y;
     int nimber, nimber_tempal = 0;
-    for(x=ncol-1;x>=0;x--)
+    for(x=ncol-1;x>=0;x--)//commercer par le dernier case,vertical
     {
         nimber = nimber_tempal;
-        for(y=nlig-1;y>=0;y--)
+        for(y=nlig-1;y>=0;y--)//commercer par le dernier case,honrizontal
         {
             nim[x][y]=nimber;
             nimber++;
-            nimber%=3;
+            nimber%=3;//modulo pour cotroler inferieur � 3
         }
         nimber_tempal+=2;
         nimber_tempal%=3;
@@ -191,74 +151,127 @@ void Calcul_Nimber(int nim[][VMAX],int nlig,int ncol)
 
 }
 
-struct T_Case Coup_Joueur(struct T_Case pion, T_Tab_Case ban, int nlig, int ncol)
+void Voisines(int nlig, int ncol, int nban, int *nb_vois, T_Tab_Case *vois, T_Tab_Case T, struct T_Case _case)
 {
-    int i, choice;
-    T_Tab_Case *t;
-    printf("A ton tour");
-    Voisines(nlig, ncol, nban, t, ban, pion);
-    printf("hello");
+    int i,j,k;
+    int bannie=0; //bannie=0 quand pas banni
+    *nb_vois=0;
+    for(i=(_case.x)+1; i<=(_case.x)+2; i++) //boucle qui teste pour les 2 cases en dessous de la case du pion
+        {
+            if(nban!=0) //s'il y a des cases bannies
+            {
+                for(j = 0; j <= nban; j++)
+                {
+                    if((T.tab[j].x==i) && (T.tab[j].y==_case.y))
+                    {
+                        bannie=1; //bannie prend la valeur 1
+                        break;
+                    }
+                }
+            }
+            if((bannie == 0)&&(i <= nlig))
+            {
+                (*nb_vois)++; //incr�mentation de nb_vois
+                (*vois).tab[*nb_vois-1].x=i; //stockage de la coordonn�e x de la case voisine
+                (*vois).tab[*nb_vois-1].y=_case.y; //stockage de la coordonn�e y de la case voisine
+            }
+        }
+    bannie=0;
+    for(k=(_case.y)+1; k<=(_case.y)+2; k++)//boucle qui teste pour les 2 cases � droite de la case du pion
+        {
+            if(nban!=0) //s'il y a des cases bannies
+            {
+                for(j = 0; j <= nban; j++)
+                {
+                    if((T.tab[j].x==_case.x) && (T.tab[j].y==k))
+                    {
+                        bannie=1; //bannie prend la valeur 1
+                        break;
+                    }
+                }
+            }
+            if((bannie == 0)&&(k <= ncol))
+            {
+                (*nb_vois)++; //incr�mentation de nb_vois
+                (*vois).tab[*nb_vois-1].x=_case.x; //stockage de la coordonn�e x de la case voisine
+                (*vois).tab[*nb_vois-1].y=k; //stockage de la coordonn�e y de la case voisine
+            }
+        }
+}
 
-    if(t->taille==1)
+struct T_Case Coup_Joueur(struct T_Case pion, T_Tab_Case ban, int nlig, int ncol, int nban)
+{
+    int i, choice, nb_vois;
+    T_Tab_Case vois;
+    printf("A ton tour!\n");
+    Voisines(nlig, ncol, nban, &nb_vois, &vois, ban, pion);
+    if(nb_vois==1)
     {
-        printf("Il y a une seule possibilite: (%d,%d)\nvalider --->", t->tab[0].x, t->tab[0].y);
+        printf("Il n'y a qu'une seule possibilite: (%d,%d)\nvalider --->", vois.tab[0].x, vois.tab[0].y);
         scanf("%d", &choice);
-        pion.x=t->tab[0].x;
-        pion.y=t->tab[0].y;
+        pion.x=vois.tab[0].x;
+        pion.y=vois.tab[0].y;
     }
-
     else
     {
         printf("Veuillez choisir la destination: ");
-        for(i=0; i < t->taille; i++)
+        for(i = 1; i <= nb_vois; i++)
         {
-            printf("%d: (%d,%d)", i, t->tab[t->taille-1].x, t->tab[t->taille-1].y);
+            printf("%d: (%d,%d) ", i, vois.tab[i-1].x, vois.tab[i-1].y);
         }
         do
         {
             printf("\n--->");
-            Lire_Entier(1, t->taille);
+            while(getchar()!='\n');
+            scanf("%d", &choice);
         }
-        while(choice<1 || choice>t->taille);
+        while(choice<1 || choice>nb_vois);
+        pion.x=vois.tab[choice-1].x;
+        pion.y=vois.tab[choice-1].y;
     }
-    pion.x=t->tab[choice-1].x;
-    pion.y=t->tab[choice-1].y;
-
     return pion;
 }
 
-
-
-
-
-struct T_Case Coup_Ordi_Gagnant(int nlig,int ncol,struct T_Case pion,int nim[][VMAX], T_Tab_Case *t )
+struct T_Case Coup_Ordi_Hasard(struct T_Case pion, T_Tab_Case ban, int nlig, int ncol, int nban)
 {
-    int choice,i,x,y;
-    struct T_Tab_Case *t;
-    Voisines(nlig,ncol, *t, ban, case);
-    choice=rand()% t->taille;
-    for(i=0;i<t->taille;i++)
+    int choice, nb_vois;
+    T_Tab_Case vois;
+    Voisines(nlig, ncol, nban, &nb_vois, &vois, ban, pion);
+    choice=rand()%nb_vois;
+    printf("L'ordinateur deplace le pion en: (%d,%d)\n",vois.tab[choice].x,vois.tab[choice].y);
+    pion.x=vois.tab[choice].x;
+    pion.y=vois.tab[choice].y;
+    return pion;
+}
+
+struct T_Case Coup_Ordi_Gagnant(struct T_Case pion, T_Tab_Case ban, int nlig, int ncol, int nban, int nim[][VMAX])
+{
+    int choice, i, x, y, nb_vois;
+    T_Tab_Case vois;
+    Voisines(nlig, ncol, nban, &nb_vois, &vois, ban, pion);
+    choice=rand()%nb_vois;
+    for(i=1;i<nb_vois;i++)
     {
-        if(nim[x][y]==0)
+        if(nim[vois.tab[i].x-1][vois.tab[i].y-1==0])
         {
             choice=i;
             break;
         }
     }
-    pion.x=t->tab[choice].x;
-    pion.y=t->tab[choice].y;
-    printf("L'ordinateur deplace le pion en (%d,%d)\n",t->tab[choice].x, t->tab[choice].y);
+    pion.x=vois.tab[choice].x;
+    pion.y=vois.tab[choice].y;
+    printf("L'ordinateur deplace le pion en (%d,%d)\n",vois.tab[choice].x, vois.tab[choice].y);
     return pion;
 }
-struct T_Case Coup_Ordi_Hasard(struct T_Case pion,int nlig,int ncol)
-{
-    int choice;
-    struct T_Tab_Case *t;
-    Voisines( );
-    choice=rand()%t->taille;
-    printf("L'ordinateur deplace le pion en(%d,%d)\n",t->tab[choice].x,t->tab[choice].y);
-    pion.x=t->tab[choice].x;
-    pion.y=t->tab[choice].y;
-    return pion;
-}
+
+
+
+
+
+
+
+
+
+
+
 
